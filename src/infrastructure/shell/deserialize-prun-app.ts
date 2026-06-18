e4f43064887f24ee4fc6e14a3b513bb860a4e6d8
Miles Baskin <milburbas@gmail.com>
2026-06-18 15:53:10 +0900

    initial experimentation with autocomplete injection

--- src/infrastructure/shell/deserialize-prun-app.ts
+++ src/infrastructure/shell/deserialize-prun-app.ts
@@ -13,3 +13,41 @@ for (let i = 0; i < scripts.length; i++) {
     script.remove();
   }
 }
+const origMap = Array.prototype.map;
+
+const proxyMap = function <T, U>(
+  this: T[],
+  callbackfn: (value: T, index: number, array: T[]) => U,
+  thisArg?: unknown,
+): U[] {
+  try {
+    if (this.length > 0 && this.some(x => x && typeof x === 'object' && 'panelDefinition' in x)) {
+      // Array.prototype.map = origMap;
+      // this.map = proxyMap;
+      console.log('ACMPL Found commands list:', this);
+      const mapped = origMap.call(this, callbackfn, thisArg) as U[];
+      const sample = mapped[0] as object;
+      if (!('cmd' in sample) || !('hidden' in sample) || !('parameters' in sample)) {
+        return mapped;
+      }
+      const mySuggestion = Object.create(Object.getPrototypeOf(sample));
+
+      Object.assign(mySuggestion, sample, {
+        _cmd: 'XIT',
+        _hidden: false,
+        _parameters: [],
+      });
+      const injected = [...mapped, mySuggestion];
+      console.log('ACMPL Mapped commands list:', mapped);
+      console.log('ACMPL My suggestion:', mySuggestion);
+      console.log('ACMPL Injected commands list:', injected);
+      return injected;
+    }
+    return origMap.call(this, callbackfn, thisArg) as U[];
+  } catch (e) {
+    console.error('ACMPL Error:', e);
+    return origMap.call(this, callbackfn, thisArg) as U[];
+  }
+};
+
+Array.prototype.map = proxyMap;
